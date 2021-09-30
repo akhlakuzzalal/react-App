372d4d994bd8bafd02a6c488459b03abba412303
Akhlak Uzzalal <akhlakuzzalal@gmail.com>
2021-10-01 00:09:00 +0600

    home-fixed

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
             <About></About>
           </Route>
           <Route path="*">
-            <About></About>
+            <Home></Home>
           </Route>
         </Switch>
       </BrowserRouter>
